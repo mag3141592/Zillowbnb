9b7add935f0427c6ac452c0ed49daa78ec2844e0
Agustin Lew <ablew@uw.edu>
2019-05-22 14:20:29 -0700

    Changed which features to convert to matrix

--- convert_to_matrix.py
+++ convert_to_matrix.py
@@ -25,8 +25,23 @@ def metadata(data_frame):
     dict_ng = {neighborhood_group[i]:i for i in range(len(neighborhood_group))}
     dict_pt = {property_type[i]:i for i in range(len(property_type))}
     dict_rt = {room_type[i]:i for i in range(len(room_type))}
+    columns = ['neighbourhood_cleansed', 'neighbourhood_group_cleansed',
+               'latitude', 'longitude', 'property_type', 'room_type',
+               'accommodates', 'bathrooms', 'bedrooms', 'beds', 'amenities_TV',
+               'amenities_Heating', 'amenities_Air conditioning', 'amenities_Breakfast',
+               'amenities_Laptop friendly workspace', 'amenities_Indoor fireplace',
+               'amenities_Iron', 'amenities_Hair dryer', 'amenities_Private entrance',
+               'amenities_Smoke detector', 'amenities_Carbon monoxide detector',
+               'amenities_First aid kit', 'amenities_Fire extinguisher',
+               'amenities_Lock on bedroom door', 'amenities_Pool',
+               'amenities_Kitchen', 'amenities_Washer', 'amenities_Dryer',
+               'amenities_Free parking on premises', 'amenities_Elevator',
+               'amenities_Hot tub', 'amenities_Gym', 'amenities_Pets allowed',
+               'amenities_Smoking allowed', 'amenities_Suitable for events',
+               'amenities_Pets live on this property', 'price']
+
     return {'neighborhood' : dict_n, 'neighborhood group' : dict_ng, "property type" : dict_pt,
-            "room type" : dict_rt}
+            "room type" : dict_rt, "columns" : columns}
 
 def to_matrix(data_frame):
     """
@@ -40,10 +55,7 @@ def to_matrix(data_frame):
     if len(data_frame.columns) != 296:
         raise ValueError("Dataframe must have 296 columns")
 
-    df2 = data_frame[['neighbourhood_cleansed', 'neighbourhood_group_cleansed', 'latitude',
-                      'longitude', 'property_type', 'room_type', 'accommodates', 'bathrooms',
-                      'bedrooms', 'beds']
-                     + list(data_frame.columns[94:279]) + ['price']]
+    df2 = data_frame[metadata(data_frame)['columns']]
     df2 = df2.dropna()
 
     df2["neighbourhood_cleansed"].replace(metadata(data_frame)['neighborhood'], inplace=True)
