97585b14a9362c4d2cc256d6bc70e4b8914a005d
Hannah Murphy <hmurph3@uw.edu>
2019-05-28 19:15:03 -0700

    fixing some styling errors

--- ZillowBnb/test/unittest.py
+++ ZillowBnb/test/unittest.py
@@ -1,31 +1,34 @@
-import os
+"""
+This module runs unit tests for ZillowBnb
+"""
 import unittest
-from ZillowBnb import ZillowBnb
+from ZillowBnb import get_data
+from ZillowBnb import get_calendar_summary
 
 class UnitTest(unittest.TestCase):
     """
     This class runs all the unit tests for ZillowBnb
     """
 
-    def test_calendar_no_na():
-    """
-    Tests calendar.csv.gz data has no empty values
-    """
-    data = get_data.download_dataset('seattle', 'wa', 'united-states',
-                                     '2019-04-15', 'calendar.csv.gz')
-    assert data.isnull.values.any()
+    def test_calendar_no_na(self):
+        """
+        Tests calendar.csv.gz data has no empty values
+        """
+        data = get_data.download_dataset('seattle', 'wa', 'united-states',
+                                         '2019-04-15', 'calendar.csv.gz')
+        assert data.isnull.values.any()
 
 
-    def test_calendar_more_than_one_row():
+    def test_calendar_more_than_one_row(self):
         """
         Tests calendar.csv.gz data for more than one row
         """
         data = get_data.download_dataset('seattle', 'wa', 'united-states',
                                          '2019-04-15', 'calendar.csv.gz')
-    assert data.shape[1] >= 1
+        assert data.shape[1] >= 1
 
 
-    def test_calendar_col():
+    def test_calendar_col(self):
         """
         Test calendar.csv.gz data has the proper columns for
         Seattle, WA, Unintes States
@@ -38,7 +41,7 @@ class UnitTest(unittest.TestCase):
         assert all(x in column_titles for x in data_col_titles)
 
 
-    def test_calendar_col_types():
+    def test_calendar_col_types(self):
         """
         Tests calendar.csv.gz data has the proper column data types for
         Seattle, WA, United States
@@ -52,7 +55,7 @@ class UnitTest(unittest.TestCase):
                 data.dtypes.maximum_nights == float)
 
 
-    def test_calendar_summary_col():
+    def test_calendar_summary_col(self):
         """
         Tests that get_calendar_summary produces the proper column names
         """
@@ -63,10 +66,10 @@ class UnitTest(unittest.TestCase):
                          'summer_price', 'winter_price', 'weekday_price',
                          'weekend_price']
         test_col_titles = list(test)
-        assert all(x in column_titles for x in test_col_title)
+        assert all(x in column_titles for x in test_col_titles)
 
 
-    def test_calendar_summary_col_types():
+    def test_calendar_summary_col_types(self):
         """
         Tests that get_calendar_summary produces the proper column data types
         """
@@ -78,9 +81,8 @@ class UnitTest(unittest.TestCase):
                 test.dtypes.spring_price == float and
                 test.dtypes.summer_price == float and
                 test.winter_price == float and
-                test.dtypes.weekday_price == flaot and
-                test.dtypes.weekdend_price == flaot)
-
+                test.dtypes.weekday_price == float and
+                test.dtypes.weekdend_price == float)
 
 if __name__ == '__main__':
-unittest.main()
+    unittest.main()
