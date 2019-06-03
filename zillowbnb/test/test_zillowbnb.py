f46d8c1ae8ce89d52096ab12b04ecb940215579f
Maggie Stark <maggiestark1@gmail.com>
2019-06-02 21:07:53 -0700

    switch asserttrue to assertTrue, erroring building

--- zillowbnb/test/test_zillowbnb.py
+++ zillowbnb/test/test_zillowbnb.py
@@ -5,6 +5,11 @@ import unittest
 from zillowbnb.submodule import get_data
 from zillowbnb.submodule import get_calendar_summary
 
+DATASET_PROPERTIES = {'date':'2019-04-15',
+                      'city':'Seattle',
+                      'state':'WA',
+                      'country':'United-States'}
+
 class UnitTest(unittest.TestCase):
     """
     This class runs all the unit tests for ZillowBnb
@@ -14,18 +19,16 @@ class UnitTest(unittest.TestCase):
         """
         Tests calendar.csv.gz data has no empty values
         """
-        data = get_data.download_dataset('seattle', 'wa', 'united-states',
-                                         '2019-04-15', 'calendar.csv.gz')
-        self.asserttrue(data.isnull.values.any())
+        data = get_data.download_dataset(DATASET_PROPERTIES, 'calendar.csv.gz')
+        self.assertTrue(data.isnull.values.any())
 
 
     def test_calendar_more_than_one_row(self):
         """
         Tests calendar.csv.gz data for more than one row
         """
-        data = get_data.download_dataset('seattle', 'wa', 'united-states',
-                                         '2019-04-15', 'calendar.csv.gz')
-        self.asserttrue(data.shape[1] >= 1)
+        data = get_data.download_dataset(DATASET_PROPERTIES, 'calendar.csv.gz')
+        self.assertTrue(data.shape[1] >= 1)
 
 
     def test_calendar_col(self):
@@ -33,12 +36,11 @@ class UnitTest(unittest.TestCase):
         Test calendar.csv.gz data has the proper columns for
         Seattle, WA, Unintes States
         """
-        data = get_data.download_dataset('seattle', 'wa', 'united-states',
-                                         '2019-04-15', 'calendar.csv.gz')
+        data = get_data.download_dataset(DATASET_PROPERTIES, 'calendar.csv.gz')
         column_titles = ['listing_id', 'date', 'available', 'price',
                          'adjusted_price', 'minimum_nights', 'maximum_nights']
         data_col_titles = list(data)
-        self.asserttrue(all(x in column_titles for x in data_col_titles))
+        self.assertTrue(all(x in column_titles for x in data_col_titles))
 
 
     def test_calendar_col_types(self):
@@ -46,9 +48,8 @@ class UnitTest(unittest.TestCase):
         Tests calendar.csv.gz data has the proper column data types for
         Seattle, WA, United States
         """
-        data = get_data.download_dataset('seattle', 'wa', 'united-states',
-                                         '2019-04-15', 'calendar.csv.gz')
-        self.asserttrue(data.dtypes.listing_id == int and
+        data = get_data.download_dataset(DATASET_PROPERTIES, 'calendar.csv.gz')
+        self.assertTrue(data.dtypes.listing_id == int and
                         data.dtypes.date == str and
                         data.dtypes.available == str and
                         data.dtypes.price == str and
@@ -61,24 +62,22 @@ class UnitTest(unittest.TestCase):
         """
         Tests that get_calendar_summary produces the proper column names
         """
-        data = get_data.download_dataset('seattle', 'wa', 'united-states',
-                                         '2019-04-15', 'calendar.csv.gz')
+        data = get_data.download_dataset(DATASET_PROPERTIES, 'calendar.csv.gz')
         test = get_calendar_summary.create_calendar_price_averages(data)
         column_titles = ['listing_id', 'fall_price', 'spring_price',
                          'summer_price', 'winter_price', 'weekday_price',
                          'weekend_price']
         test_col_titles = list(test)
-        self.asserttrue(all(x in column_titles for x in test_col_titles))
+        self.assertTrue(all(x in column_titles for x in test_col_titles))
 
 
     def test_calendar_summary_col_types(self):
         """
         Tests that get_calendar_summary produces the proper column data types
         """
-        data = get_data.download_dataset('seattle', 'wa', 'united-states',
-                                         '2019-04-15', 'calendar.csv.gz')
+        data = get_data.download_dataset(DATASET_PROPERTIES, 'calendar.csv.gz')
         test = get_calendar_summary.create_calendar_price_averages(data)
-        self.asserttrue(test.dtypes.listing_id == int and
+        self.assertTrue(test.dtypes.listing_id == int and
                         test.dtypes.fall_price == float and
                         test.dtypes.spring_price == float and
                         test.dtypes.summer_price == float and
