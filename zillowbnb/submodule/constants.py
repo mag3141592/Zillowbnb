1c66f11e554687c44632377a3a3e054795ada150
Maggie Stark <maggiestark1@gmail.com>
2019-06-04 23:40:05 -0700

    Adding constants submodule. Updating get_data.py and unittests

---
+++ zillowbnb/submodule/constants.py
@@ -0,0 +1,35 @@
+"""
+Contains contants used throughout package
+"""
+import os
+
+DATA_FOLDER = os.path.abspath('../../data')  + '/'
+
+DATE = 'date'
+CITY = 'city'
+STATE = 'state'
+COUNTRY = 'country'
+DATASET_PROPERTIES = {DATE:'2019-04-15',
+                      CITY:'Seattle',
+                      STATE:'WA',
+                      COUNTRY:'United-States'}
+
+LISTINGS_DATA = 'listings.csv.gz'
+REVIEWS_DATA = 'reviews.csv.gz'
+CALENDAR_DATA = 'calendar.csv.gz'
+
+LISTING_COLUMNS = ['id', 'neighbourhood_cleansed', 'neighbourhood_group_cleansed',
+                   'latitude', 'longitude', 'property_type', 'room_type',
+                   'minimum_nights', 'maximum_nights',
+                   'accommodates', 'bathrooms', 'bedrooms', 'beds', 'amenities_TV',
+                   'amenities_Heating', 'amenities_Air conditioning', 'amenities_Breakfast',
+                   'amenities_Laptop friendly workspace', 'amenities_Indoor fireplace',
+                   'amenities_Iron', 'amenities_Hair dryer', 'amenities_Private entrance',
+                   'amenities_Smoke detector', 'amenities_Carbon monoxide detector',
+                   'amenities_First aid kit', 'amenities_Fire extinguisher',
+                   'amenities_Lock on bedroom door', 'amenities_Pool',
+                   'amenities_Kitchen', 'amenities_Washer', 'amenities_Dryer',
+                   'amenities_Free parking on premises', 'amenities_Elevator',
+                   'amenities_Hot tub', 'amenities_Gym', 'amenities_Pets allowed',
+                   'amenities_Smoking allowed', 'amenities_Suitable for events',
+                   'amenities_Pets live on this property', 'price']
---
+++ zillowbnb/test/test_get_data.py
@@ -0,0 +1,66 @@
+"""
+Tests the input and output of get_data functions
+"""
+
+import unittest
+from os.path import dirname, abspath, join
+import sys
+
+# Find code directory relative to our directory
+THIS_DIR = dirname(__file__)
+CODE_DIR = abspath(join(THIS_DIR, '..', 'submodule'))
+sys.path.append(CODE_DIR)
+
+import constants # pylint: disable=E0401, C0413
+import get_data # pylint: disable=E0401, C0413
+
+class UnitTest(unittest.TestCase):
+    """
+    5 unit test
+    """
+
+    def test_input_dictionary(self):
+        """
+        Tests input dictionary has required keys
+        """
+        dataset_properties = {'d':'2019-04-15',
+                              'c':'Seattle',
+                              's':'WA'}
+        file = 'listings.csv.gz'
+        with self.assertRaises(ValueError):
+            get_data.download_dataset(dataset_properties, file)
+
+    def test_valid_url(self):
+        """
+        Tests for valid data URL
+        """
+        file = 'listing.csv.gz'
+        with self.assertRaises(ValueError):
+            get_data.download_dataset(constants.DATASET_PROPERTIES, file)
+
+    def test_output_shape(self):
+        """
+        Tests output dataframe has both rows and columns
+        """
+        data = get_data.download_dataset(constants.DATASET_PROPERTIES,
+                                         constants.REVIEWS_DATA)
+        rows, cols = list(data.shape)
+        self.assertTrue(rows > 0 and cols > 0)
+
+    def test_files_exist(self):
+        """
+        Tests input files exist
+        """
+        with self.assertRaises(FileNotFoundError):
+            get_data.merge_data('test1.csv', 'test2.csv', 'test3.csv', 'listing_id')
+
+    def test_files_share_col(self):
+        """
+        Tests input files all have provided column to merge on
+        """
+        with self.assertRaises(ValueError):
+            get_data.merge_data('clean_listings.csv', 'reviews_sa_summarized.csv',
+                                'calendar_price_averages.csv', 'test')
+
+if __name__ == '__main__':
+    unittest.main()
